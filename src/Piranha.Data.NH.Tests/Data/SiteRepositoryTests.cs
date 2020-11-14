32952d0659e7d5bd71da860323ee73601761c534
Manfred Lange <manfredmlange@gmail.com>
2020-11-14 04:25:04 +0000

    SiteRepository.Save()

--- src/Piranha.Data.NH.Tests/Data/SiteRepositoryTests.cs
+++ src/Piranha.Data.NH.Tests/Data/SiteRepositoryTests.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using NHibernate;
 using NUnit.Framework;
+using Piranha.Models;
 using RimuTec.Piranha.Data.NH.DataAccess;
 
 namespace RimuTec.Piranha.Data.NH.Repositories
@@ -20,6 +21,21 @@ namespace RimuTec.Piranha.Data.NH.Repositories
             var repository = new SiteRepository(SessionFactory);
             var sites = await repository.GetAll().ConfigureAwait(false);
             Assert.AreEqual(0, sites.Count(s => s.Id == siteId));
+        }
+
+        [Test]
+        public async Task Save()
+        {
+            var internalId = Guid.NewGuid().ToString("N");
+            var site = new Site {
+                Description = $"Site description {internalId}",
+                InternalId = internalId,
+                Title = $"Title {internalId}"
+            };
+            var repository = new SiteRepository(SessionFactory);
+            await repository.Save(site).ConfigureAwait(false);
+            var sites = await repository.GetAll().ConfigureAwait(false);
+            Assert.AreEqual(1, sites.Count(s => s.InternalId == internalId));
         }
 
         private ISessionFactory SessionFactory { get; }
