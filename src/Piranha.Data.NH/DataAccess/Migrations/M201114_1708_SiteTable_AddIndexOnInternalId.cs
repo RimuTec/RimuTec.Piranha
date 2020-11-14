47b37a1daf66b1bb9dcf695245a18543082861cf
Manfred Lange <manfredmlange@gmail.com>
2020-11-14 04:16:01 +0000

    Add index on Site -> InternalId

---
+++ src/Piranha.Data.NH/DataAccess/Migrations/M201114_1708_SiteTable_AddIndexOnInternalId.cs
@@ -0,0 +1,16 @@
+using FluentMigrator;
+
+namespace RimuTec.Piranha.Data.NH.DataAccess.Migrations
+{
+    [Migration(201114_1708)]
+    public class M201114_1708_SiteTable_AddIndexOnInternalId : UpOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.Index("IDX_Site_InternalId")
+                .OnTable("Site")
+                .OnColumn("InternalId")
+                ;
+        }
+    }
+}
