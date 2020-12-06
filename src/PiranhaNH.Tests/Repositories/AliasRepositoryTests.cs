f7f478c0d61aa6387d8428971133a6ff8984fb09
Manfred Lange <manfredmlange@gmail.com>
2020-12-06 18:25:31 +0000

    Fixed intendentation to 3

--- src/PiranhaNH.Tests/Repositories/AliasRepositoryTests.cs
+++ src/PiranhaNH.Tests/Repositories/AliasRepositoryTests.cs
@@ -8,135 +8,135 @@ using RimuTec.PiranhaNH.DataAccess;
 
 namespace RimuTec.PiranhaNH.Repositories
 {
-    [TestFixture]
-    public class AliasRepositoryTests
-    {
-        public AliasRepositoryTests()
-        {
-            SessionFactory = Database.CreateSessionFactory();
-        }
+   [TestFixture]
+   public class AliasRepositoryTests
+   {
+      public AliasRepositoryTests()
+      {
+         SessionFactory = Database.CreateSessionFactory();
+      }
 
-        [Test]
-        public async Task GetAll_ForSpecificNonExistingSite()
-        {
-            var siteId = Guid.NewGuid();
-            var repository = new AliasRepository(SessionFactory);
-            var aliases = await repository.GetAll(siteId).ConfigureAwait(false);
-            Assert.AreEqual(0, aliases.Count());
-        }
+      [Test]
+      public async Task GetAll_ForSpecificNonExistingSite()
+      {
+         var siteId = Guid.NewGuid();
+         var repository = new AliasRepository(SessionFactory);
+         var aliases = await repository.GetAll(siteId).ConfigureAwait(false);
+         Assert.AreEqual(0, aliases.Count());
+      }
 
-        [Test]
-        public async Task SaveGetAll_HappyPath()
-        {
-            Site siteModelRetrieved = await MakeSite().ConfigureAwait(false);
+      [Test]
+      public async Task SaveGetAll_HappyPath()
+      {
+         Site siteModelRetrieved = await MakeSite().ConfigureAwait(false);
 
-            var aliasModel = new Alias
-            {
-                SiteId = siteModelRetrieved.Id,
-                AliasUrl = "blog.example.com",
-                RedirectUrl = "www.example.com/blog",
-                Type = RedirectType.Temporary
-            };
-            var aliasRepository = new AliasRepository(SessionFactory);
-            await aliasRepository.Save(aliasModel).ConfigureAwait(false);
-            var aliases = await aliasRepository.GetAll(siteModelRetrieved.Id).ConfigureAwait(false);
-            Assert.AreEqual(1, aliases.Count());
-        }
+         var aliasModel = new Alias
+         {
+            SiteId = siteModelRetrieved.Id,
+            AliasUrl = "blog.example.com",
+            RedirectUrl = "www.example.com/blog",
+            Type = RedirectType.Temporary
+         };
+         var aliasRepository = new AliasRepository(SessionFactory);
+         await aliasRepository.Save(aliasModel).ConfigureAwait(false);
+         var aliases = await aliasRepository.GetAll(siteModelRetrieved.Id).ConfigureAwait(false);
+         Assert.AreEqual(1, aliases.Count());
+      }
 
-        [Test]
-        public async Task Save_SetsId()
-        {
-            Site siteModelRetrieved = await MakeSite().ConfigureAwait(false);
+      [Test]
+      public async Task Save_SetsId()
+      {
+         Site siteModelRetrieved = await MakeSite().ConfigureAwait(false);
 
-            var aliasModel = new Alias
-            {
-                SiteId = siteModelRetrieved.Id,
-                AliasUrl = "blog.example.com",
-                RedirectUrl = "www.example.com/blog",
-                Type = RedirectType.Temporary
-            };
-            var aliasRepository = new AliasRepository(SessionFactory);
-            await aliasRepository.Save(aliasModel).ConfigureAwait(false);
-            Assert.False(aliasModel.Id == Guid.Empty);
-        }
+         var aliasModel = new Alias
+         {
+            SiteId = siteModelRetrieved.Id,
+            AliasUrl = "blog.example.com",
+            RedirectUrl = "www.example.com/blog",
+            Type = RedirectType.Temporary
+         };
+         var aliasRepository = new AliasRepository(SessionFactory);
+         await aliasRepository.Save(aliasModel).ConfigureAwait(false);
+         Assert.False(aliasModel.Id == Guid.Empty);
+      }
 
-        [Test]
-        public async Task Delete()
-        {
-            var aliasId = Guid.NewGuid().ToString("N");
-            Site siteModelRetrieved = await MakeSite().ConfigureAwait(false);
-            var aliasModel = new Alias
-            {
-                SiteId = siteModelRetrieved.Id,
-                AliasUrl = "blog.example.com",
-                RedirectUrl = $"www.example.com/blog?id={aliasId}",
-                Type = RedirectType.Temporary
-            };
-            var aliasRepository = new AliasRepository(SessionFactory);
-            await aliasRepository.Save(aliasModel).ConfigureAwait(false);
-            var aliases = await aliasRepository.GetAll(siteModelRetrieved.Id).ConfigureAwait(false);
-            var toDelete = aliases.First(x => x.RedirectUrl.Contains(aliasId));
-            await aliasRepository.Delete(toDelete.Id).ConfigureAwait(false);
-            aliases = await aliasRepository.GetAll(siteModelRetrieved.Id).ConfigureAwait(false);
-            Assert.False(aliases.Any(x => x.RedirectUrl.Contains(aliasId)));
-        }
+      [Test]
+      public async Task Delete()
+      {
+         var aliasId = Guid.NewGuid().ToString("N");
+         Site siteModelRetrieved = await MakeSite().ConfigureAwait(false);
+         var aliasModel = new Alias
+         {
+            SiteId = siteModelRetrieved.Id,
+            AliasUrl = "blog.example.com",
+            RedirectUrl = $"www.example.com/blog?id={aliasId}",
+            Type = RedirectType.Temporary
+         };
+         var aliasRepository = new AliasRepository(SessionFactory);
+         await aliasRepository.Save(aliasModel).ConfigureAwait(false);
+         var aliases = await aliasRepository.GetAll(siteModelRetrieved.Id).ConfigureAwait(false);
+         var toDelete = aliases.First(x => x.RedirectUrl.Contains(aliasId));
+         await aliasRepository.Delete(toDelete.Id).ConfigureAwait(false);
+         aliases = await aliasRepository.GetAll(siteModelRetrieved.Id).ConfigureAwait(false);
+         Assert.False(aliases.Any(x => x.RedirectUrl.Contains(aliasId)));
+      }
 
-        [Test]
-        public async Task GetById()
-        {
-            var aliasId = Guid.NewGuid().ToString("N");
-            Site siteModelRetrieved = await MakeSite().ConfigureAwait(false);
-            string redirectUrl = $"www.example.com/blog?id={aliasId}";
-            var aliasModel = new Alias
-            {
-                SiteId = siteModelRetrieved.Id,
-                AliasUrl = "blog.example.com",
-                RedirectUrl = redirectUrl,
-                Type = RedirectType.Temporary
-            };
-            var aliasRepository = new AliasRepository(SessionFactory);
-            await aliasRepository.Save(aliasModel).ConfigureAwait(false);
-            var retrievedAlias = await aliasRepository.GetById(aliasModel.Id).ConfigureAwait(false);
-            Assert.AreEqual(aliasModel.Id, retrievedAlias.Id);
-            Assert.AreEqual(redirectUrl, retrievedAlias.RedirectUrl);
-        }
+      [Test]
+      public async Task GetById()
+      {
+         var aliasId = Guid.NewGuid().ToString("N");
+         Site siteModelRetrieved = await MakeSite().ConfigureAwait(false);
+         string redirectUrl = $"www.example.com/blog?id={aliasId}";
+         var aliasModel = new Alias
+         {
+            SiteId = siteModelRetrieved.Id,
+            AliasUrl = "blog.example.com",
+            RedirectUrl = redirectUrl,
+            Type = RedirectType.Temporary
+         };
+         var aliasRepository = new AliasRepository(SessionFactory);
+         await aliasRepository.Save(aliasModel).ConfigureAwait(false);
+         var retrievedAlias = await aliasRepository.GetById(aliasModel.Id).ConfigureAwait(false);
+         Assert.AreEqual(aliasModel.Id, retrievedAlias.Id);
+         Assert.AreEqual(redirectUrl, retrievedAlias.RedirectUrl);
+      }
 
-        [Test]
-        public async Task GetByAliasUrl()
-        {
-            var aliasId = Guid.NewGuid().ToString("N");
-            Site siteModelRetrieved = await MakeSite().ConfigureAwait(false);
-            string redirectUrl = $"www.example.com/blog?id={aliasId}";
-            string aliasUrl = $"blog.example.com?q={aliasId}";
-            var aliasModel = new Alias
-            {
-                SiteId = siteModelRetrieved.Id,
-                AliasUrl = aliasUrl,
-                RedirectUrl = redirectUrl,
-                Type = RedirectType.Temporary
-            };
-            var aliasRepository = new AliasRepository(SessionFactory);
-            await aliasRepository.Save(aliasModel).ConfigureAwait(false);
-            var retrievedAlias = await aliasRepository.GetByAliasUrl(aliasUrl, siteModelRetrieved.Id).ConfigureAwait(false);
-            Assert.AreEqual(aliasModel.Id, retrievedAlias.Id);
-            Assert.AreEqual(redirectUrl, retrievedAlias.RedirectUrl);
-            Assert.AreEqual(aliasUrl, retrievedAlias.AliasUrl);
-        }
+      [Test]
+      public async Task GetByAliasUrl()
+      {
+         var aliasId = Guid.NewGuid().ToString("N");
+         Site siteModelRetrieved = await MakeSite().ConfigureAwait(false);
+         string redirectUrl = $"www.example.com/blog?id={aliasId}";
+         string aliasUrl = $"blog.example.com?q={aliasId}";
+         var aliasModel = new Alias
+         {
+            SiteId = siteModelRetrieved.Id,
+            AliasUrl = aliasUrl,
+            RedirectUrl = redirectUrl,
+            Type = RedirectType.Temporary
+         };
+         var aliasRepository = new AliasRepository(SessionFactory);
+         await aliasRepository.Save(aliasModel).ConfigureAwait(false);
+         var retrievedAlias = await aliasRepository.GetByAliasUrl(aliasUrl, siteModelRetrieved.Id).ConfigureAwait(false);
+         Assert.AreEqual(aliasModel.Id, retrievedAlias.Id);
+         Assert.AreEqual(redirectUrl, retrievedAlias.RedirectUrl);
+         Assert.AreEqual(aliasUrl, retrievedAlias.AliasUrl);
+      }
 
-        private async Task<Site> MakeSite()
-        {
-            var siteInternalId = $"{Guid.NewGuid()}";
-            var siteRepository = new SiteRepository(SessionFactory);
-            var siteModelToCreate = new Site
-            {
-                InternalId = siteInternalId,
-                Description = "Site 42",
-                Title = "Title 42",
-            };
-            await siteRepository.Save(siteModelToCreate).ConfigureAwait(false);
-            return await siteRepository.GetByInternalId(siteInternalId).ConfigureAwait(false);
-        }
+      private async Task<Site> MakeSite()
+      {
+         var siteInternalId = $"{Guid.NewGuid()}";
+         var siteRepository = new SiteRepository(SessionFactory);
+         var siteModelToCreate = new Site
+         {
+            InternalId = siteInternalId,
+            Description = "Site 42",
+            Title = "Title 42",
+         };
+         await siteRepository.Save(siteModelToCreate).ConfigureAwait(false);
+         return await siteRepository.GetByInternalId(siteInternalId).ConfigureAwait(false);
+      }
 
-        private ISessionFactory SessionFactory { get; }
-    }
+      private ISessionFactory SessionFactory { get; }
+   }
 }
--- src/PiranhaNH/Repositories/AliasRepository.cs
+++ src/PiranhaNH/Repositories/AliasRepository.cs
@@ -10,108 +10,108 @@ using RimuTec.PiranhaNH.Entities;
 
 namespace RimuTec.PiranhaNH.Repositories
 {
-    public class AliasRepository : RepositoryBase, IAliasRepository
-    {
-        public AliasRepository(ISessionFactory sessionFactory) : base(sessionFactory)
-        {
-        }
+   public class AliasRepository : RepositoryBase, IAliasRepository
+   {
+      public AliasRepository(ISessionFactory sessionFactory) : base(sessionFactory)
+      {
+      }
 
-        public async Task Delete(Guid id)
-        {
-            await InTx(async session =>
-            {
-                var toDelete = await session.GetAsync<AliasEntity>(id).ConfigureAwait(false);
-                await session.DeleteAsync(toDelete).ConfigureAwait(false);
-            })
-            .ConfigureAwait(false);
-        }
+      public async Task Delete(Guid id)
+      {
+         await InTx(async session =>
+         {
+            var toDelete = await session.GetAsync<AliasEntity>(id).ConfigureAwait(false);
+            await session.DeleteAsync(toDelete).ConfigureAwait(false);
+         })
+         .ConfigureAwait(false);
+      }
 
-        public async Task<IEnumerable<Alias>> GetAll(Guid siteId)
-        {
-            return await InTx(async session =>
+      public async Task<IEnumerable<Alias>> GetAll(Guid siteId)
+      {
+         return await InTx(async session =>
+         {
+            var aliases = new List<Alias>();
+            var entities = await session.Query<AliasEntity>()
+                                           .Where(a => a.Site.Id == siteId)
+                                           .ToListAsync()
+                                           .ConfigureAwait(false);
+            aliases.AddRange(entities.Select(entity => new Alias
             {
-                var aliases = new List<Alias>();
-                var entities = await session.Query<AliasEntity>()
-                                            .Where(a => a.Site.Id == siteId)
-                                            .ToListAsync()
-                                            .ConfigureAwait(false);
-                aliases.AddRange(entities.Select(entity => new Alias
-                {
-                    Id = entity.Id,
-                    AliasUrl = entity.AliasUrl,
-                    Created = entity.Created,
-                    LastModified = entity.LastModified,
-                    RedirectUrl = entity.RedirectUrl,
-                    SiteId = entity.Site.Id,
-                    Type = entity.Type
-                }));
-                return aliases;
+               Id = entity.Id,
+               AliasUrl = entity.AliasUrl,
+               Created = entity.Created,
+               LastModified = entity.LastModified,
+               RedirectUrl = entity.RedirectUrl,
+               SiteId = entity.Site.Id,
+               Type = entity.Type
+            }));
+            return aliases;
+         }
+         ).ConfigureAwait(false);
+      }
+
+      public async Task<Alias> GetByAliasUrl(string url, Guid siteId)
+      {
+         return await InTx(async session =>
+         {
+            var entities = await session.Query<AliasEntity>()
+                                           .Where(a => a.Site.Id == siteId
+                                           && a.AliasUrl == url)
+                                           .ToListAsync()
+                                           .ConfigureAwait(false);
+            if (entities.Count > 0)
+            {
+               var entity = entities[0];
+               return new Alias
+               {
+                  Id = entity.Id,
+                  AliasUrl = entity.AliasUrl,
+                  Created = entity.Created,
+                  LastModified = entity.LastModified,
+                  RedirectUrl = entity.RedirectUrl,
+                  SiteId = entity.Site.Id,
+                  Type = entity.Type
+               };
             }
-            ).ConfigureAwait(false);
-        }
+            return null;
+         }).ConfigureAwait(false);
+      }
 
-        public async Task<Alias> GetByAliasUrl(string url, Guid siteId)
-        {
-            return await InTx(async session =>
+      public async Task<Alias> GetById(Guid id)
+      {
+         return await InTx(async session =>
+         {
+            var entity = await session.GetAsync<AliasEntity>(id).ConfigureAwait(false);
+            return new Alias
             {
-                var entities = await session.Query<AliasEntity>()
-                                            .Where(a => a.Site.Id == siteId
-                                            && a.AliasUrl == url)
-                                            .ToListAsync()
-                                            .ConfigureAwait(false);
-                if (entities.Count > 0)
-                {
-                    var entity = entities[0];
-                    return new Alias
-                    {
-                        Id = entity.Id,
-                        AliasUrl = entity.AliasUrl,
-                        Created = entity.Created,
-                        LastModified = entity.LastModified,
-                        RedirectUrl = entity.RedirectUrl,
-                        SiteId = entity.Site.Id,
-                        Type = entity.Type
-                    };
-                }
-                return null;
-            }).ConfigureAwait(false);
-        }
+               Id = entity.Id,
+               AliasUrl = entity.AliasUrl,
+               Created = entity.Created,
+               LastModified = entity.LastModified,
+               RedirectUrl = entity.RedirectUrl,
+               SiteId = entity.Site.Id,
+               Type = entity.Type
+            };
+         }).ConfigureAwait(false);
+      }
 
-        public async Task<Alias> GetById(Guid id)
-        {
-            return await InTx(async session =>
-            {
-                var entity = await session.GetAsync<AliasEntity>(id).ConfigureAwait(false);
-                return new Alias
-                {
-                    Id = entity.Id,
-                    AliasUrl = entity.AliasUrl,
-                    Created = entity.Created,
-                    LastModified = entity.LastModified,
-                    RedirectUrl = entity.RedirectUrl,
-                    SiteId = entity.Site.Id,
-                    Type = entity.Type
-                };
-            }).ConfigureAwait(false);
-        }
+      public Task<IEnumerable<Alias>> GetByRedirectUrl(string url, Guid siteId)
+      {
+         throw new NotImplementedException();
+      }
 
-        public Task<IEnumerable<Alias>> GetByRedirectUrl(string url, Guid siteId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task Save(Alias model)
-        {
-            await InTx(async session =>
-            {
-                AliasEntity entity = await session.GetAsync<AliasEntity>(model.Id).ConfigureAwait(false) ?? new AliasEntity();
-                entity.Site = await session.GetAsync<SiteEntity>(model.SiteId).ConfigureAwait(false);
-                entity.AliasUrl = model.AliasUrl;
-                entity.RedirectUrl = model.RedirectUrl;
-                entity.Type = model.Type;
-                await session.SaveOrUpdateAsync(entity).ConfigureAwait(false);
-                model.Id = entity.Id;
-            }).ConfigureAwait(false);
-        }
-    }
+      public async Task Save(Alias model)
+      {
+         await InTx(async session =>
+         {
+            AliasEntity entity = await session.GetAsync<AliasEntity>(model.Id).ConfigureAwait(false) ?? new AliasEntity();
+            entity.Site = await session.GetAsync<SiteEntity>(model.SiteId).ConfigureAwait(false);
+            entity.AliasUrl = model.AliasUrl;
+            entity.RedirectUrl = model.RedirectUrl;
+            entity.Type = model.Type;
+            await session.SaveOrUpdateAsync(entity).ConfigureAwait(false);
+            model.Id = entity.Id;
+         }).ConfigureAwait(false);
+      }
+   }
 }
